66eeca856caa0414a4de1b1abc3a0341dbaba3e8
1pervysage0 <57150979+1pervysage0@users.noreply.github.com>
2021-06-19 00:17:45 +0530

    Update script.js
    my simple interest java script

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+
+
+		function simple(){
+
+	var p=document.getElementById("principal").value;
+	var r=document.getElementById("interest").value;
+	var t=document.getElementById("time").value;
+	var si=0;
+	var s=0;
+	
+	
+       si=parseInt((p*r*t)/100);
+       s=parseInt(+si + +p);
+     
+       document.getElementById('num1').innerHTML="simple interest:"+si+" and total amount will be "+s;
+      
+       }
+	
+	
+
+	
